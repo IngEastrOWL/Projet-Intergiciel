7993778245d7c78a784b3ee522d6b6a0e826f6cc
IngEastrOWL <antoine.texier3@gmail.com>
2026-05-04 15:34:03 +0200

    Added shm channel impl

--- go/shm/Channel.java
+++ go/shm/Channel.java
@@ -5,23 +5,45 @@ import go.Observer;
 
 public class Channel<T> implements go.Channel<T> {
 
-    public Channel(String name) {
-        // TODO
+    private String name;
+    private T value;
+    private boolean hasValue = false;
+
+    public Channel(String name) { this.name = name; }
+    
+    public synchronized void out(T v) {
+        try {
+            while (hasValue) { // tant que le canal est occupé par un out, on attend
+                wait();
+            }
+            value = v;
+            hasValue = true;
+            notifyAll();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            notifyAll();
+        }
     }
     
-    public void out(T v) {
-        // TODO
-    }
-    
-    public T in() {
-        // TODO
-        return null;
+    public synchronized T in() {
+        try {
+            while (!hasValue) { // tant que le canal est vide, on attend
+                wait();
+            }
+            T res = value;
+            hasValue = false;
+            notifyAll();
+            return res;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return null;
+        } finally {
+            notifyAll();
+        }
     }
 
-    public String getName() {
-        // TODO
-        return null;
-    }
+    public String getName() { return this.name; }
 
     public void observe(Direction dir, Observer observer) {
         // TODO
