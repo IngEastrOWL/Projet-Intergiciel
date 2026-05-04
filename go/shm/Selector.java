dad4df4cc90026e1c37b5c3cda1852f8f936ad69
IngEastrOWL <antoine.texier3@gmail.com>
2026-05-04 17:56:08 +0200

    Selector.java first idea, not working

--- go/shm/Selector.java
+++ go/shm/Selector.java
@@ -2,18 +2,44 @@ package go.shm;
 
 import go.Direction;
 import go.Channel;
+import go.Observer;
+
 import java.util.Map;
 import java.util.Set;
 
 public class Selector implements go.Selector {
 
+    private Map<Channel, Direction> channels;
+    private Channel selectedChannel = null;
+
     public Selector(Map<Channel, Direction> channels) {
-        // TODO
+        this.channels = channels;
     }
 
     public Channel select() {
-        // TODO
-        return null;
+        synchronized (this) {
+            for (Map.Entry<Channel, Direction> entry : channels.entrySet()) {
+                entry.getKey().observe(Direction.inverse(entry.getValue()), new Observer() {
+                    @Override
+                    public void update() {
+                        synchronized (Selector.this) {
+                            if (selectedChannel == null) {
+                                selectedChannel = entry.getKey();
+                                Selector.this.notify();
+                            }
+                        }
+                    }
+                });
+                try {
+                    while (selectedChannel == null) {
+                        wait();
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            return selectedChannel;
+        }
     }
 
 }
